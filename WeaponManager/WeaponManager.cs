1939c3210f7a6edccc07bb73bd7ff2fe4f542943
faketuna <faketuna.st@gmail.com>
2024-08-01 16:57:21 +0900

    feat: initial commit

---
+++ WeaponManager/WeaponManager.cs
@@ -0,0 +1,14 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace WeaponManager;
+
+public class WeaponManager : BasePlugin
+{
+    public override string ModuleName => "Weapon Manager";
+
+    public override string ModuleVersion => "0.0.1";
+
+    public override string ModuleAuthor => "faketuna";
+
+    public override string ModuleDescription => "Provides advanced weapon management feature";
+}
